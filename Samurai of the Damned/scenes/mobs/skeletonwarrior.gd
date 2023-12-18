27ed25ecf0b8ba01ee60253125a89d8afdf0e987
itsrmborromeo <rheamaeborromeo123@gmail.com>
2023-12-18 19:44:34 +0800

    init

--- Samurai of the Damned/scenes/mobs/skeletonwarrior.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-@export var ch : CharacterBody2D
-@export var an : AnimatedSprite2D
-func _physics_process(delta):
-	ch.move_and_slide()
--- Samurai of the Damned/ui/game_screen.gd
+++
@@ -1,5 +0,0 @@
-extends CanvasLayer
-
-
-func _on_pause_texture_button_pressed():
-	GameManager.pause_game()
